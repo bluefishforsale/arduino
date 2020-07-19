01ba5a368a2e6e7fc9a3cb63ea10759f88f7afc7
tskiens-quizlet <terrac.skien@quizlet.com>
2020-07-19 11:56:16 -0700

    fix some knob parameters, and add a fade chase

--- game_loop_frame/game_loop_frame.ino
+++ game_loop_frame/game_loop_frame.ino
@@ -1,6 +1,9 @@
 #include <Adafruit_NeoPixel.h>
+
 #define LED_PIN 10
 #define NUM_LEDS 291
+#define NUM_SIDES 3
+#define SIDE_LENGTH NUM_LEDS/NUM_SIDES
 #define MS_PER_FRAME  33  // 30fps
 #define REP 3
 #define COLOR_POT_PIN 14
@@ -19,13 +22,20 @@ int potMax = 1023; //the highest value that comes out of the potentiometers
 
 uint32_t color;  
 uint8_t decay_trail;
-uint32_t i = 0;    
+uint32_t h, i, j, k = 0;    
+
+
+uint8_t sin_lut[256];
 
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 
 void setup() { 
+  for (i=0; i < sizeof(sin_lut); i++){
+    sin_lut[i] = (uint8_t) 256 * sinf(TWO_PI * i / sizeof(sin_lut) );
+  }
+  Serial.begin(9600);
   strip.begin();
   strip.show();
 }
@@ -36,8 +46,12 @@ void loop() {
   while (true) {
     double current = millis();
     processInput();
-    trails();
-//    twinkle();
+
+///// Effects
+//    trails();
+    twinkle();
+//    fadingTrails();
+
     render();
     double elapsed = current - previous;
     previous = current;
@@ -46,17 +60,8 @@ void loop() {
 }
 
 
-void processInput() {
-  decay_pot_val  = analogRead(DECAY_POT_PIN);
-  color_pot_val  = analogRead(COLOR_POT_PIN);
-  
-  scaled_decay_val = map(decay_pot_val, potMin, potMax, 0, 100);
-  decay_trail = scaled_decay_val;
 
-  scaled_color_val = map(color_pot_val, potMin, potMax, 0, 256);
-  color = ColorWheel(scaled_color_val);
-}
-
+/////////////////////////  Effects functions
 
 void trails() {
   // fade brightness all LEDs one step
@@ -77,26 +82,91 @@ void trails() {
   i=(i+1) %NUM_LEDS;
 }
 
+
+
 void twinkle() {
   // fade brightness all LEDs by decay amount
-  for (int j=0; j <= NUM_LEDS; j++) {
-      if( random(0,100) >= decay_trail) {
-        fadeToBlack(j, decay_trail );        
+  for (int j=0; j < NUM_LEDS; j++) {
+      if( random(0,100) < decay_trail*1.5) {
+//        fadeToBlack(j, map(decay_trail, 0, SIDE_LENGTH, 128, 1) );  // we need to sample the pixel color here
+        fadeToBlack(j, decay_trail );  // we need to sample the pixel color here
       }
   }
 
   // draw j random bright spots
-  for ( int j=0; j <= decay_trail ; j++) {
-    if( random(0,100) <= decay_trail) {
+  for ( int j=0; j < decay_trail/2; j++) {
+    if( random(0,100) > decay_trail) {
       i = random(0,NUM_LEDS);
-      strip.setPixelColor(i, color);
+      strip.setPixelColor(i, fadeColor(color, map(decay_trail, 0, SIDE_LENGTH, 256, 1) ));  // no need to sample color, just reduce brightness proportional to how many we show
+//      strip.setPixelColor(i, fadeColor(color, decay_trail) );  // no need to sample color, just reduce brightness proportional to how many we show
     }
   }
-
 }
+
+
+
+void fadingTrails() {
+ 
+  // fade brightness all LEDs one step
+  for(int j=0; j<NUM_LEDS; j++) {
+    if( random(0,100) < (decay_trail/random(1,10)) ) {
+      fadeToBlack(j, decay_trail*2 );        
+    }
+  }
+   
+  // draw pixel
+  for(int j = 0; j < decay_trail; j++) {
+    for (int k=1; k <= REP; k++) { // generate most bright *spark*
+
+      color = fadeColor(color, i  % SIDE_LENGTH);
+      strip.setPixelColor((OFFSET *k +i) %NUM_LEDS, color );            // forward 
+
+      color = fadeColor(color, i % SIDE_LENGTH );
+      strip.setPixelColor((NUM_LEDS -i +(OFFSET*k)) %NUM_LEDS, color ); // reverse
+    }
+  }
+  // increment pixel and loop back around
+  i=(i+1) %NUM_LEDS;
+}
+
+
+/////////////////////////  common utility functions
 
 void render() {
   strip.show();
+}
+
+
+void processInput() {
+  decay_pot_val  = analogRead(DECAY_POT_PIN);
+  color_pot_val  = analogRead(COLOR_POT_PIN);
+  
+  scaled_decay_val = map(decay_pot_val, potMin, potMax, 1, 99   );
+  decay_trail = scaled_decay_val;
+
+  scaled_color_val = map(color_pot_val, potMin, potMax, 1, 255);
+  color = ColorWheel( scaled_color_val );
+
+//  for (i=0; i < sizeof(sin_lut); i++){
+//    Serial.printf("%d, ", sin_lut[i]);
+//  }
+//  Serial.println();
+
+}
+
+
+uint32_t fadeColor(uint32_t oldColor, byte fadeValue) {
+  uint8_t r, g, b;
+ 
+  r = (oldColor & 0x00ff0000UL) >> 16;
+  g = (oldColor & 0x0000ff00UL) >> 8;
+  b = (oldColor & 0x000000ffUL);
+
+  r=(r<=10)? 0 : (int) r-(r*fadeValue/256);
+  g=(g<=10)? 0 : (int) g-(g*fadeValue/256);
+  b=(b<=10)? 0 : (int) b-(b*fadeValue/256);
+
+  return strip.Color(r,g,b);
 }
 
 
